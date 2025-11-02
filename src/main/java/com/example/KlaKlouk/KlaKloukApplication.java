52b3fb397e5c196ed534928ff3453bc10a0bd4ca
ChenChanveasna <2024023chen@aupp.edu.kh>
2025-11-02 21:46:07 +0700

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/KlaKlouk/KlaKloukApplication.java
@@ -0,0 +1,13 @@
+package com.example.KlaKlouk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlaKloukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlaKloukApplication.class, args);
+	}
+
+}
